a3d71ec875d0f9a1e616c0c140a97c27444d8f47
Bolarinwa Ajayi <ajayibcd@gmail.com>
2021-02-20 22:34:32 +0100

    modified design

--- Desktop/Recharge-card/script.js
+++ Desktop/Recharge-card/script.js
@@ -1,4 +1,8 @@
 function rand(){
-   var randNum = Math.floor(Math.random()* 1000000000);
+   var randNum = Math.floor(Math.random()* 1000000000000000);
+   var x = Math.floor(Math.random() * 10);
+   console.log(x);
+   randNum += x;
+   console.log(randNum);
    document.getElementById("pin").value = randNum;
 }
