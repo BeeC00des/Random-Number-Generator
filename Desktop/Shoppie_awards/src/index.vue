7d77044c0dc186ff89a4674cbe74e8653c8a0dcd
Bolarinwa Ajayi <ajayibcd@gmail.com>
2021-05-09 17:03:58 +0100

    first commit

---
+++ Desktop/Shoppie_awards/src/index.vue
@@ -0,0 +1,14 @@
+<template>
+   <h1 class="main-text">Build this shit!</h1>
+</template>
+<style  scoped>
+   .main-text{
+      color:red;
+      font-size:30px;
+   }
+</style>
+<script>
+export default {
+   
+}
+</script>
